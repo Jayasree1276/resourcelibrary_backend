f6a7b9af78d71d04410d3cef1249d703ff83b43b
PAILA VYSHNAVI <2400030741@kluniversity.in>
2026-04-10 09:52:12 +0530

    Delete full_stack_2_2-backend directory

--- full_stack_2_2-backend/resource-library-backend/src/main/java/com/example/library/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.library.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
