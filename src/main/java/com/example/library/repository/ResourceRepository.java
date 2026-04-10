7a51a4f493b0e48b6a33ad088e16f2e242836fea
PAILA VYSHNAVI <2400030741@kluniversity.in>
2026-04-10 09:42:34 +0530

    Delete src directory

--- src/main/java/com/example/library/repository/ResourceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.library.model.Resource;
-
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
